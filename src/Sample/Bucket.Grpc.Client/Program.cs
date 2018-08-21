b3cbeddd23e9f68bdb3c60dfa94438e5164685bd
tl <tl@DESKTOP-OLH7OBH>
2018-08-21 17:39:31 +0800

    grpc client增加服务发现地址获取

--- src/Sample/Bucket.Grpc.Client/Program.cs
+++ src/Sample/Bucket.Grpc.Client/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Bucket.Gprc.Client;
 using Bucket.Gprc.Extensions;
+using Bucket.LoadBalancer;
+using Bucket.LoadBalancer.Extensions;
+using Bucket.ServiceDiscovery.Extensions;
+using Bucket.ServiceDiscovery.Consul;
 using MagicOnion;
 using MagicOnion.Client;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +26,10 @@ namespace Bucket.Grpc.Client
             configuration = builder.Build();
             // 添加DI容器
             var services = new ServiceCollection();
+            services.AddLoadBalancer();
             services.AddGrpcClient();
+            // 添加服务发现
+            services.AddServiceDiscovery(build => { build.UseConsul(configuration); });
             // 容器
             serviceProvider = services.BuildServiceProvider();
             // 日志使用
@@ -33,14 +40,20 @@ namespace Bucket.Grpc.Client
 
             Console.WriteLine("Hello World!");
 
+            var _loadBalancerHouse = serviceProvider.GetRequiredService<ILoadBalancerHouse>();
             var _rpcChannelFactory = serviceProvider.GetRequiredService<IGrpcChannelFactory>();
-            var channel = _rpcChannelFactory.Get("10.10.133.235", 5004);
+            // 服务发现地址
+            var endpoints = _loadBalancerHouse.Get("Bucket.Grpc.Server").Result;
+            var endpoint = endpoints.Lease().Result;
+            var channel = _rpcChannelFactory.Get(endpoint.Address, endpoint.Port);
+            // var channel = _rpcChannelFactory.Get("10.10.133.235", 5004);
+            var hello = MagicOnionClient.Create<IHello>(channel);
             var StartTime = DateTime.Now;     
             for (var i = 0; i < 10000; i++)
             {
-                var hel = MagicOnionClient.Create<IHello>(channel);
-                var result = hel.Hello("haha test");
-                Console.WriteLine(result.GetAwaiter().GetResult());
+                hello = MagicOnionClient.Create<IHello>(channel);
+                var result = hello.Hello("haha test").GetAwaiter().GetResult();
+                Console.WriteLine($"grpc 请求结果:{result}");
             }
             var TimeLength = Math.Round((DateTime.Now - StartTime).TotalMilliseconds, 4);
             Console.WriteLine("GRPC 10000次请求耗时" + TimeLength + "毫秒");
