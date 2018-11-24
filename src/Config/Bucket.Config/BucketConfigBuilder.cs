21aa2c2b07c53aab628e38fa9f41d348ac74e4d6
tianliang <tianliang@axon.com.cn>
2018-11-24 13:54:39 +0800

    重构配置中新客户端,增加扩展方式

---
+++ src/Config/Bucket.Config/BucketConfigBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bucket.Config
+{
+    public class BucketConfigBuilder : IBucketConfigBuilder
+    {
+        public IServiceCollection Services { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+
+        public BucketConfigBuilder(IServiceCollection services, IConfiguration configurationRoot)
+        {
+            Configuration = configurationRoot;
+            Services = services;
+        }
+    }
+}
---
+++ src/Config/Bucket.Config/IBucketConfigBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Bucket.Config
+{
+    public interface IBucketConfigBuilder
+    {
+        IServiceCollection Services { get; }
+
+        IConfiguration Configuration { get; }
+    }
+}
