4d185cc3ebf00863e7c27084197427e4b4aa8834
tianliang <tianliang@axon.com.cn>
2018-11-27 17:58:11 +0800

    IUser增加Ids

--- src/Bucket.Core/IUser.cs
+++ src/Bucket.Core/IUser.cs
@@ -8,6 +8,7 @@ namespace Bucket.Core
     public interface IUser
     {
         string Id { get; }
+        string Ids { get; }
         string Name { get; }
         string MobilePhone { get; }
         IEnumerable<Claim> Claims { get; }
