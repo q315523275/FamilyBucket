18a372a778295eda0a6ab13cd3852bb5d24b24e7
tianliang <tianliang@axon.com.cn>
2019-10-17 19:25:09 +0800

    IEventBus接口增加非json数据事件订阅接口

--- src/EventBus/Bucket.EventBus/Abstractions/IEventBus.cs
+++ src/EventBus/Bucket.EventBus/Abstractions/IEventBus.cs
@@ -10,9 +10,15 @@ namespace Bucket.EventBus.Abstractions
             where T : IntegrationEvent
             where TH : IIntegrationEventHandler<T>;
 
+        void SubscribeDynamic<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+
         void Unsubscribe<T, TH>()
             where TH : IIntegrationEventHandler<T>
             where T : IntegrationEvent;
+
+        void UnsubscribeDynamic<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
 
         void StartSubscribe();
     }
