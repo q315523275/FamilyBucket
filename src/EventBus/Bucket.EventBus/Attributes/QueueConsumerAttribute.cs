f803795e5f2b4e302e4ee8b995a3095a8bedb9ee
tianliang <tianliang@axon.com.cn>
2020-04-08 14:33:25 +0800

    EventBus事件与处理器管理增加自定义EventName属性兼容

--- src/EventBus/Bucket.EventBus/Attributes/QueueConsumerAttribute.cs
+++ src/EventBus/Bucket.EventBus/Attributes/QueueConsumerAttribute.cs
@@ -9,12 +9,21 @@ namespace Bucket.EventBus.Attributes
         {
             get { return _queueName; }
         }
+        public int PrefetchCount
+        {
+            get { return _prefetchCount; }
+        }
 
         private string _queueName { get; set; }
-
+        private int _prefetchCount { get; set; }
         public QueueConsumerAttribute(string queueName)
         {
             _queueName = queueName;
+        }
+        public QueueConsumerAttribute(string queueName, int prefetchCount = 1)
+        {
+            _queueName = queueName;
+            _prefetchCount = prefetchCount;
         }
     }
 }
--- src/EventBus/Bucket.EventBus/Implementation/InMemoryEventBusSubscriptionsManager.cs
+++ src/EventBus/Bucket.EventBus/Implementation/InMemoryEventBusSubscriptionsManager.cs
@@ -1,8 +1,10 @@
 ﻿using Bucket.EventBus.Abstractions;
+using Bucket.EventBus.Attributes;
 using Bucket.EventBus.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Bucket.EventBus.Implementation
 {
@@ -154,7 +156,12 @@ namespace Bucket.EventBus.Implementation
 
         public string GetEventKey<T>()
         {
-            return typeof(T).Name;
+            var eventType = typeof(T);
+            var eventNameAttr = eventType.GetCustomAttribute<EventNameAttribute>();
+            if (eventNameAttr != null)
+                return eventNameAttr.EventName;
+            else
+                return eventType.Name;
         }
     }
 }
